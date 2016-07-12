b5443446e7eb7c52ad18ce76a3333b0cd6341081
Adam <Adam@Adam-PC>
2016-07-12 14:07:19 -0500

    Initial load

---
+++ PromoAnalytics/ClassSource/DataPrep.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 12, 2016
+
+@author: Adam
+'''
+#comment
+
+class DataPrep(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
