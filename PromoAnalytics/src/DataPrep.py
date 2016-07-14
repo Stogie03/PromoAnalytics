7f87941f39d74cfc6c915b9e298504d9bdfd1cd5
astokes <astokes@ASTOKES-T14N.Perseco.com>
2016-07-14 11:45:00 -0500

    small change

--- PromoAnalytics/src/DataPrep.py
+++ PromoAnalytics/src/DataPrep.py
@@ -57,46 +57,53 @@ class DataPrep(object):
             print("There was an error loading the Data frame")
             
         #test the non promo column names
-        
-        if (np.array_equal(stdColNames,df_names[0:15])):    #need to fix this as it errors out
-            stColNamesPass=True
-        else:
-            stColNamesPass=False
-        
-        #test the number of promo columns
-        
-        df_pnames=df_names[15:df_names.size]  #get the promo columns from the dataframe
-        possibleValues=np.zeros((limit,limit))
-        
-        for x in range(1,limit):
-        
-            for z in range(1,limit):
-                if z<2 and x < 2:
-                   possibleValues[x,z] = int(comb(z,1,exact=False)) 
-                if z >= 2 and z >= x:
-                    possibleValues[x,z] = int(possibleValues[x-1,z] + comb(z,x,exact=False))
-                   
-        if df_pnames.size in possibleValues[:, :]:
-            print ("VALUE FOUND")
-            print df_pnames.size
-            print possibleValues.index(df_pnames.size)
-        else:
-            print ("VALUE NOT FOUND :(")
-            print df_pnames.size
-                
-        #print df_pnames
-        #print df_names
-        #print possibleValues
-        print ('Do the standard variables pass? %s' % (stColNamesPass))
+        if df_exists==True:
+            
+            if (np.array_equal(stdColNames,df_names[0:15])):    #need to fix this as it errors out
+                stColNamesPass=True
+            else:
+                stColNamesPass=False
+            
+            #test the number of promo columns
+            
+            df_pnames=df_names[15:df_names.size]  #get the promo columns from the dataframe
+            possibleValues=np.zeros((limit,limit))
+            
+            for x in range(1,limit):
+            
+                for z in range(1,limit):
+                    if z<2 and x < 2:
+                       possibleValues[x,z] = int(comb(z,1,exact=False)) 
+                    if z >= 2 and z >= x:
+                        possibleValues[x,z] = int(possibleValues[x-1,z] + comb(z,x,exact=False))
+                       
+            if df_pnames.size in possibleValues[:, :]:
+                print ("VALUE FOUND")
+                print df_pnames.size
+                print possibleValues.index(df_pnames.size)
+            else:
+                print ("VALUE NOT FOUND :(")
+                print df_pnames.size
+                    
+            #print df_pnames
+            #print df_names
+            #print possibleValues
+            print ('Do the standard variables pass? %s' % (stColNamesPass))
         
         #this is the boolean to say the validation passed
         objInitDataVal = True
             
     
-#TEST THE CLASS
+#TEST THE CLASS here
 testClass=DataPrep('C:\Users\\astokes\Desktop\Analytic Solutions\mldata_2012_to_2016.csv',None,'CSV')    
 
 '''
+for idx, val in enumerate(possibleValues):
+    if 3 in val[:]:
+        for idx2, val2 in enumerate(val):
+            if val2==3:
+                print(idx,idx2)
+
     #ALL Functions below here will probably be part of another package/class
     def formatTS(self,inpu_df):
         a=6
